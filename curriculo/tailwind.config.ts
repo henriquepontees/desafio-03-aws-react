3db4e836d3bead502869fb8e84f8d7c0b07ff2bb
Henrique Pontes <henriquepontes0405@gmail.com>
2024-11-06 11:28:23 -0300

    feat: add tailwind configs

--- curriculo/tailwind.config.ts
+++ curriculo/tailwind.config.ts
@@ -9,11 +9,24 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary_text: "rgba(24, 25, 31, 1)",
+        secondary_text: "rgba(255, 255, 255, 1)",
+        tertiary_text: "rgba(209, 213, 219, 1)",
+        primary_color: "rgba(9, 188, 138, 1)",
+        secondary_color: "rgba(0, 67, 70, 1)",
+        card_color: "rgba(80, 137, 145, 1)",
+        dark_green: "rgba(23, 42, 58, 1)",
+        just_red: "rgba(153, 32, 32, 1)",
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      },
+      lineHeight: {
+        '130': '1.3',
       },
     },
   },
   plugins: [],
 };
+
 export default config;
